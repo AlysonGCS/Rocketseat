98ebd609ae16a726a8610e84db9c42cf666eccf1
AlysonGCS <alyson.gabriel1707@hotmail.com>
2026-08-23 22:56:12 -0300

    class JS

--- Javascrpit/classroom/scripts.js
+++ Javascrpit/classroom/scripts.js
@@ -1,6 +1,21 @@
 console.log("Hello Word!");
 
 
+let nome = 'Alyson'
 
-let nome = 'Alyson';
 console.log(nome);
+
+let age = 17
+
+console.log(age >= 18 ? "Você pode dirigir!" : "Você não pode dirigir!")
+
+let hour = 19
+
+if (hour <= 12){
+    console.log("Bom dia!")
+} else if (hour >=12 && hour <= 18){
+    console.log("Boa tarde!")
+}else{
+    console.log("Boa noite!")
+}
+
